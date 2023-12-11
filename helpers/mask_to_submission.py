741aa9dafc9553b63b58ea5bedd9bfeb7c3fb63c
jinyangp <jinyangpeh@gmail.com>
2023-12-11 20:45:24 +0100

    script to generate submission

--- helpers/mask_to_submission.py
+++ helpers/mask_to_submission.py
@@ -36,11 +36,11 @@ def masks_to_submission(submission_filename, *image_filenames):
             f.writelines('{}\n'.format(s) for s in mask_to_submission_strings(fn))
 
 
-if __name__ == '__main__':
-    submission_filename = 'dummy_submission.csv'
-    image_filenames = []
-    for i in range(1, 51):
-        image_filename = 'training/groundtruth/satImage_' + '%.3d' % i + '.png'
-        print image_filename
-        image_filenames.append(image_filename)
-    masks_to_submission(submission_filename, *image_filenames)
+# if __name__ == '__main__':
+#     submission_filename = 'submission.csv'
+#     image_filenames = []
+#     for i in range(1, 51):
+#         image_filename = 'training/groundtruth/satImage_' + '%.3d' % i + '.png'
+#         print(image_filename)
+#         image_filenames.append(image_filename)
+#     masks_to_submission(submission_filename, *image_filenames)
